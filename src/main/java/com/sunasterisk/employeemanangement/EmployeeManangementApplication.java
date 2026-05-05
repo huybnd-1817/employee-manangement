9759c99638b112a2ecfba74327e29fad84ad9a43
nguyen.duc.huyb <nguyen.duc.huy-b@sun-asterisk.com>
2026-05-05 11:50:03 +0700

    Initial commit

---
+++ src/main/java/com/sunasterisk/employeemanangement/EmployeeManangementApplication.java
@@ -0,0 +1,13 @@
+package com.sunasterisk.employeemanangement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeManangementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeManangementApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/sunasterisk/employeemanangement/EmployeeManangementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunasterisk.employeemanangement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeManangementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
